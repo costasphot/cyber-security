009d854657c58b7d70a9f09d6b4dd0cb0a2dccc6
Costas Phot <georgephotsch@gmail.com>
2026-06-28 00:50:55 +0300

    Add the PostgreSQL database connection helper.

--- postgresql_injection/common.php
+++ postgresql_injection/common.php
@@ -1,4 +1,6 @@
 <?php
+
+use Pdo\Pgsql;
   /**
    * common.php
    * 
@@ -10,7 +12,11 @@
 
   define ("APP_TITLE", "PostgreSQL Injection");
 
-  define("DATABASE_NAME", "web_development");
+  define("DATABASE_HOST", "localhost");
+  define("DATABASE_PORT", "5432");
+  define("DATABASE_NAME", "sql_injection");
+  define("DATABASE_USERNAME", "postgres");
+  define("DATABASE_PASSWORD", "postgres");
 
   define("TABLE_USERS",   "Users");
   define("TABLE_PRODUCT", "Product");
@@ -19,5 +25,22 @@
 
   function EscapeHtml(string $value): string {
     return htmlspecialchars($value, ENT_QUOTES, "UTF-8");
+  }
+
+  function ConnectToDatabase(): \PgSql\Connection {
+    $connection_string = 
+        "host="      . DATABASE_HOST .
+        " port="     . DATABASE_PORT .
+        " dbname="   . DATABASE_NAME .
+        " user="     . DATABASE_USERNAME .
+        " password=" . DATABASE_PASSWORD;
+
+    $connection = pg_connect($connection_string);
+
+    if ($connection === false) {
+      die("[Error] Failed to connect to the PostgreSQL database.");
+    }
+
+    return $connection;
   }
 ?>
